76749af29033742ad0e47caca221da3d43c465cb
Liangcai Chen <ChenL47@cardiff.ac.uk>
2018-11-05 13:46:42 +0000

    install redux & react-redux, and some book data

---
+++ src/containers/BookList.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+
+class BookList extends Component {
+    
+}
---
+++ src/reducers/Books_reducer..js
@@ -0,0 +1,8 @@
+export default () => (
+    [
+        {title: 'Python programming'},
+        {title: 'Java Programming'},
+        {title: 'Hardware'},
+        {title: 'Harry Potter'},
+    ]
+)
